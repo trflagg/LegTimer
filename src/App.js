895870d01e42864540d79c6fe10484c2fe66f625
Taylor Flagg <trflagg@gmail.com>
2019-02-12 21:05:42 -0500

    Add cue seconds input

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ const LengthInput = styled.input`
   width: 3em;
 `;
 
+const CueSeconds = styled.input`
+  width: 4em;
+`;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       lengthInMinutes: 6,
+      cueSeconds: 60,
     };
   }
 
@@ -19,12 +24,19 @@ class App extends Component {
     });
   }
 
+  handleCueSecondsChange = event => {
+    this.setState({
+      cueSeconds: event.currentTarget.value,
+    });
+  }
+
   render() {
-    const { lengthInMinutes } = this.state;
+    const { cueSeconds, lengthInMinutes } = this.state;
     return (
       <div>
         <h1>Leg Timer</h1>
         <p>Run for <LengthInput value={lengthInMinutes} onChange={this.handleLengthChange} type='number'/> minutes</p>
+        <p>Every <CueSeconds value={cueSeconds} onChange={this.handleCueSecondsChange} type='number' /> seconds play sound.</p>
       </div>
     );
   }

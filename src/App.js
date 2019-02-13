08fc4fc9c596c3a36dd37580ea757795736a3dd1
Taylor Flagg <trflagg@gmail.com>
2019-02-12 20:40:30 -0500

    Add lengh input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const LengthInput = styled.input`
+  width: 3em;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lengthInMinutes: 6,
+    };
+  }
+
+  handleLengthChange = event => {
+    this.setState({
+      lengthInMinutes: event.currentTarget.value,
+    });
+  }
+
   render() {
+    const { lengthInMinutes } = this.state;
     return (
       <div>
         <h1>Leg Timer</h1>
+        <p>Run for <LengthInput value={lengthInMinutes} onChange={this.handleLengthChange} type='number'/> minutes</p>
       </div>
     );
   }
